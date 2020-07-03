b8cba389f2a5f5af5a05ca8897c60f550158145c
Molkys Di Maria <androk.x@gmail.com>
2020-07-03 00:15:22 -0400

    Update ResourcesController.java
    Adding "api" as part of the resources controller requests

--- ms-infra-oauthresources/src/main/java/com/example/infra/oauth/resources/controller/ResourcesController.java
+++ ms-infra-oauthresources/src/main/java/com/example/infra/oauth/resources/controller/ResourcesController.java
@@ -2,9 +2,11 @@ package com.example.infra.oauth.resources.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ResourcesController {
 
 	@GetMapping("/hello")
